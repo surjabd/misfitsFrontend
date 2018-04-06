db44115da01d99e09e7671e275a0298f0e8feaa7
Hasan Ferdouse Sharif <hasan.sharif.87@gmail.com>
2018-04-05 22:40:17 -0400

    Task done. Need to add comment and make UI better

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# misfitstest
+# misfitstestr
+
+Run the following command
+
+#npm install
+#npm start
+
+Create an user account from Signup or use the followwing access
+#Username: surja
+#Password: 1234
+
+#Process Flow:
+First Register User
+Than Login User
+User can than press the button to start transmission of data from five smaple device.
+Each device will send data on a 5 sec interval.
+Data is generated from JS script(emulating accelerometer device) and sent using Socket to Django Backend Server.
+The Server saves the data and than sends the data to dashboard for live reporting. The graph is updated live through socket connect from database to client browser.
+The program uses REST API for Login and Authentiction and user Sockets for live data transmission
--- src/components/DataDashboard.js
+++ src/components/DataDashboard.js
@@ -4,39 +4,124 @@ import {connect} from "react-redux";
 import {userLogin} from "../actions/authActions";
 import {Link,BrowserRouter,Route,Switch,withRouter} from "react-router-dom";
 import $ from 'jquery';
+import {ScatterChart, Scatter, XAxis, YAxis, ZAxis, CartesianGrid, Tooltip, Legend} from 'recharts';
+
+const chartdata = {
+    device1:[
+        {device_id : 'device1',x: 15, y: 30, z: 100},
+        {device_id : 'device1',x: 30, y: 10, z: 100}
+        ],
+    device2:[
+        {device_id : 'device2',x: 15, y: 30, z: 100}
+        ],
+    device3:[
+        {device_id : 'device3',x: 30, y: 30, z: 100}
+    ],
+    device4:[
+        {device_id : 'device4',x: 40, y: 40, z: 100}
+    ],
+    device5:[
+        {device_id : 'device5',x: 50, y: 50, z: 100}
+    ]
+
+
+};
+
+
 
 class DataDashboard extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            myrender: '1'
+        };
 
         this.sendSocketData = this.sendSocketData.bind(this);
+        this.stopdata=this.stopdata.bind(this);
     }
+
 
 
     componentWillMount(){
         // console.log( this.props.authReducer)
         console.log(this.props.authReducer)
-        this.props.authReducer.auth? this.sendSocketData():this.props.history.push("/");
-
+        this.props.authReducer.auth?null:this.props.history.push("/");
+        console.log(chartdata)
 
     }
 
-    sendSocketData(){
+    sendSocketData(device){
         var socket = new WebSocket("ws://127.0.0.1:8000/ws/chat/test/");
+
+        //Opens Socket
         socket.onopen = function () {
             console.log("alerting you");
-            socket.send(JSON.stringify({'message':'Coming from Web'}));
-            // socket.send({'message':'Coming from Web'});
+            //Sending Socket data at intervals
             setInterval(() => {
-                socket.send(JSON.stringify({'message':'Coming from Web'}));;
-            }, 3000);
+                socket.send(JSON.stringify({
+                    'message':'data from server',
+                    'device_id':device,
+                    'data_x':(Math.floor(Math.random() * 50) + 1),
+                    'data_y':(Math.floor(Math.random() * 50) + 1),
+                    'data_z':(Math.floor(Math.random() * 50) + 1)
+                }));;
+            }, 5000);
         };
+
+        //Handles when a socket data is received
+        socket.onmessage = function (event){
+            var msg= JSON.parse(event.data);
+            var datatemp = {
+                "device_id" : msg.device_id,
+                "x" : msg.data_x,
+                "y" : msg.data_y,
+                "z" : msg.data_z,
+            };
+
+            if(msg.device_id==device){
+            chartdata[device].push(datatemp);
+            console.log(chartdata);
+            this.setState({
+                myrender: device
+            });
+            }
+        }.bind(this);
+
+
+    }
+    stopdata(){
+        for (var i = 1; i < 99999; i++)
+            window.clearInterval(i);
     }
 
     render() {
         return (
-            <div className="login">
+            <div className="databoard">
                 We are now in Data Dashboard
+                <ScatterChart classname="align-center" width={800} height={600} margin={{top: 20, right: 20, bottom: 20, left: 20}}>
+                    <XAxis type="number" dataKey={'x'} name='x-axis'/>
+                    <YAxis type="number" range={[1,50]}dataKey={'y'} name='y-axis'/>
+                    <ZAxis type="number" dataKey={'z'} range={[100,500]} name='z-axis'/>
+                    <CartesianGrid />
+                    <Tooltip cursor={{strokeDasharray: '3 3'}}/>
+                    <Legend />
+                    <Scatter name='A' data={chartdata['device1']} fill='#8884d8' shape="star"/>
+                    <Scatter name='B' data={chartdata['device2']} fill='#82ca9d' shape="triangle"/>
+                    <Scatter name='C' data={chartdata['device3']} fill='#82as9d' shape="square"/>
+                    <Scatter name='E' data={chartdata['device4']} fill='#82ec9d' shape="oval"/>
+                    <Scatter name='F' data={chartdata['device5']} fill='#82os9d' shape="round"/>
+                </ScatterChart>
+                <div>
+                    <button onClick={()=>{this.sendSocketData('device1')}}>Device 1</button>
+                    <button onClick={()=>{this.sendSocketData('device2')}}>Device 2</button>
+                    <button onClick={()=>{this.sendSocketData('device3')}}>Device 3</button>
+                    <button onClick={()=>{this.sendSocketData('device4')}}>Device 4</button>
+                    <button onClick={()=>{this.sendSocketData('device5')}}>Device 5</button>
+
+                    <div>
+                        <button onClick={this.stopdata}>STOP ALL DATA</button>
+                    </div>
+                </div>
             </div>
         );
     }
